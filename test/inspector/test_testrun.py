031900c4810f87cc5c5b55880aea9e38d60b4420
Stefano Lanza <steve.lanza107@tutanota.com>
2024-05-28 14:46:18 +0200

    Updated tests for TestRecord class

--- test/inspector/test_testrun.py
+++ test/inspector/test_testrun.py
@@ -1,8 +1,24 @@
 import pytest
 
 import globals
+from description.entity_description import AgentDescription
+from description.map.graph import Node
+from result.action import MoveAction
+
+test_run = globals.test_run()
 
 
 class TestTestRun:
-    def test_from_dict(self):
-        assert globals.test_run().name == "Test1"
+
+    def test_from_default_dict(self):
+        # Just to be sure the current test instance works
+        assert test_run.name == "Test1"
+        assert len(test_run.action_list) == 11
+
+    def test_getters(self):
+        assert test_run.graph.nodes == [Node(1), Node(2), Node(3), Node(4)]
+        assert AgentDescription("A1", "O1", Node(1)) in test_run.entities
+        assert test_run.action_list[4].timestep == 1
+        assert isinstance(test_run.action_list[5], MoveAction)
+
+
--- test/runner/test_testrecord.py
+++ test/runner/test_testrecord.py
@@ -15,38 +15,24 @@ class TestTestRecord:
 
     @pytest.fixture
     def test_record(self):
-        return TestRecord(test_description)
+        return TestRecord(test_description, 1)
 
     def test_getters(self, test_record):
-        assert not test_record.is_assigned()
         assert not test_record.is_done()
         assert test_record.test_name == "Test1"
         assert test_record.test == test_description
 
     def test_result_recording(self, test_record):
-        test_record.assign_test()
-        assert test_record.is_assigned()
-
         test_inspector = TestInspector(test_description)
         test_inspector.register_move(1, "T1", Node(2))
         test_inspector.register_move(2, "T1", Node(3))
 
         test_record.register_result(test_inspector.get_result())
-        assert test_record.is_assigned()
         assert test_record.is_done()
-        assert test_record.get_result()
 
     def test_operation_guards(self, test_record):
-        test_record.assign_test()
-        assert test_record.is_assigned()
-
-        with pytest.raises(OperationAlreadyDoneException) as excinfo:
-            test_record.assign_test()
-
-        assert "Test already assigned" in str(excinfo.value)
-
         with pytest.raises(ElementNotAvailableException) as excinfo:
-            test_record.get_result()
+            test_record.get_results()
 
         assert "Test has not result yet" in str(excinfo.value)
 
